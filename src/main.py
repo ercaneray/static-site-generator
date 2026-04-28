ec334bd67f19390dd625bd1908fa40e8b3b503a4
eray <erayercan1533@gmail.com>
2026-04-28 03:24:07 +0300

    Initial commit

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from pathlib import Path
 
 from markdown_to_html import extract_title, markdown_to_html_node
 
@@ -11,7 +12,7 @@ def copy_all_files(source, destination):
         print("Deleting destination...")
         shutil.rmtree(destination)
         print("Destination deleted.")
-    generate_page(source, "template.html", destination)
+
     os.mkdir(destination)
     print("New destination created.")
     items = os.listdir(source)
@@ -35,12 +36,25 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(md_file)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", content)
+    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, "w") as f:
         f.write(template)
 
 
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path)
+
+
 def main():
-    copy_all_files("static", "public")
+    print("Generating content...")
+    generate_pages_recursive("content", "template.html", "public")
 
 
 main()
